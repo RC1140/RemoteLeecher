e3e0816cf29f98efe6af908d5476dd9ee9960124
Jameel Haffejee <jameel@republiccommandos.co.za>
2010-10-18 11:28:19 +0200

    Added the ExtJs Setup file to make getting and setting up the js easy

---
+++ ExtJsSetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -d "static"]; then 
+	wget http://downloads.sencha.com/extjs/ext-3.3.0.zip
+	unzip ext-3.3.0.zip
+	cd ext-3.3.0
+	mv ext-all.js ../static
+	mv ext-all-debug.js ../static
+	mv adapter ../static
+	mv resources ../static
+else
+	echo "A static folder is required , please create the directory before running this or ensure you are in the remote leecher folder"
+fi
+
