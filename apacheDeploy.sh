afd192c68ec8a93493aca80b82f9ede67696cfb8
RC1140 <rc1140@rc1140-desktop.(none)>
2010-12-30 16:45:56 +0200

    Fixed deploy script so that you dont need to be one level above the dev folder Updated the setup script to output the basic wsgi implementation

--- apacheDeploy.sh
+++ apacheDeploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "This script requires admin access to continue"
 #This copies the current folder to the python lib folder so that it can be imported niceley :)
-cp -r RemoteLeecher/ /usr/lib/python2.6/
+if [ -d '/usr/lib/python2.6/RemoteLeecher/' ];
+then cp -r ./ /usr/lib/python2.6/RemoteLeecher/
+else mkdir /usr/lib/python2.6/RemoteLeecher/ && cp -r ./ /usr/lib/python2.6/RemoteLeecher/
+fi
+
 /etc/init.d/apache2 reload
